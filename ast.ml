19572c5a6b5d4798934ea13e5ee8801e3d18dac2
Jackson Foley <jacksoncfoley@gmail.com>
2014-11-26 15:58:23 -0500

    adds string_of_* functions for printing the AST

--- ast.ml
+++ ast.ml
@@ -59,9 +59,11 @@ type program = dfa_decl list
 (* Low-level AST printing, to help debug the structure.  These functions are
    only for debugging (the -r flag) and can be removed. *)
 
-
+(* 
 let rec expr_s = function
-   Literal(l) -> "Literal " ^ string_of_int l
+   IntLit(l) -> "IntLit" ^ string_of_int l
+ | StringLit(l) -> "StringLit" ^ l
+ | Variable(l) -> "Variable " ^ l
  | Id(s) -> "Id " ^ s
  | Binop(e1, o, e2) -> "Binop (" ^ expr_s e1 ^ ") " ^
        (match o with Add -> "Add" | Sub -> "Sub" | Mult -> "Mult" |
@@ -72,6 +74,7 @@ let rec expr_s = function
  | Call(f, es) -> "Call " ^ f ^ " [" ^
         String.concat ", " (List.map (fun e -> "(" ^ expr_s e ^ ")") es) ^ "]"
  | Noexpr -> "Noexpr"
+
 
 let rec stmt_s = function
    Block(ss) -> "Block [" ^ String.concat ",\n"
@@ -92,49 +95,88 @@ let func_decl_s f =
   "]}\n"
 
 let program_s (vars, funcs) = "([" ^ String.concat ", " vars ^ "],\n" ^
-  String.concat "\n" (List.map func_decl_s funcs) ^ ")"
+  String.concat "\n" (List.map func_decl_s funcs) ^ ")" *)
 
 (* "Pretty printed" version of the AST, meant to generate a MicroC program
    from the AST.  These functions are only for pretty-printing (the -a flag)
    the AST and can be removed. *)
+let string_of_ident = function
+    Ident(l) -> l
 
 let rec string_of_expr = function
-    Literal(l) -> string_of_int l
-  | Id(s) -> s
+    IntLit(l) -> string_of_int l
+  | StringLit(l) -> l
+  | Variable(id) -> string_of_ident id 
+  | Unop(o, e) -> 
+      string_of_expr e ^ " " ^ 
+      (match o with
+        Inc -> "++" |
+        Dec -> "--" |
+        Not -> "!"  |
+        Neg -> "neg?")
   | Binop(e1, o, e2) ->
       string_of_expr e1 ^ " " ^
       (match o with
-    Add -> "+" | Sub -> "-" | Mult -> "*" | Div -> "/"
-      | Equal -> "==" | Neq -> "!="
-      | Less -> "<" | Leq -> "<=" | Greater -> ">" | Geq -> ">=") ^ " " ^
+      Add -> "+" | Sub -> "-" | Mult -> "*" | Div -> "/"
+      | Equal -> "==" | Neq -> "!=" | Mod -> "%"
+      | Lt -> "<" | Leq -> "<=" | Gt -> ">" | Geq -> ">=" | And -> "&&" | Or -> "||" ) ^ " " ^
       string_of_expr e2
-  | Assign(v, e) -> v ^ " = " ^ string_of_expr e
-  | Call(f, el) ->
-      f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
-  | Noexpr -> ""
+  | ExprAssign(id, e) -> string_of_ident id ^ " " ^ string_of_expr e
+  | Call(id, e_list) -> string_of_ident id ^ " " ^ 
+      "(" ^ String.concat ", " (List.map string_of_expr e_list) ^ ")"
+  | Push(id, e) -> string_of_ident id ^ " " ^ string_of_expr e 
+  | Pop(id) -> string_of_ident id
+  | Peek(id) -> string_of_ident id
+  | EosLit -> "EOSLIT"
+
+let rec string_of_datatype = function
+  Datatype(vartype) -> 
+    (match vartype with 
+      Int -> "int" | String -> "String" | Stack -> "Stack" | Double -> "Double" | Void -> "Void"
+    )
+  | Stacktype(datatype) -> string_of_datatype datatype
+
+let rec string_of_decl = function
+    VarDecl(dt, id) -> string_of_datatype dt ^ " " ^ string_of_ident id
+  | VarAssignDecl(dt,id,value) -> string_of_datatype dt ^ " " ^ string_of_ident id
+      ^ " = " ^ (match value with 
+                ExprVal(e) -> string_of_expr e)
 
 let rec string_of_stmt = function
     Block(stmts) ->
       "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
   | Expr(expr) -> string_of_expr expr ^ ";\n";
   | Return(expr) -> "return " ^ string_of_expr expr ^ ";\n";
-  | If(e, s, Block([])) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s
-  | If(e, s1, s2) ->  "if (" ^ string_of_expr e ^ ")\n" ^
-      string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
-  | For(e1, e2, e3, s) ->
-      "for (" ^ string_of_expr e1  ^ " ; " ^ string_of_expr e2 ^ " ; " ^
-      string_of_expr e3  ^ ") " ^ string_of_stmt s
-  | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
+  | Assign(id, expr) -> string_of_ident id ^ " = " ^ string_of_expr expr
+  (* | If(e, s, Block([])) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s *)
+  (* | If(e, s1, s2) ->  "if (" ^ string_of_expr e ^ ")\n" ^ *)
+      (* string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2 *)
+  (* | For(e1, e2, e3, s) -> *)
+      (* "for (" ^ string_of_expr e1  ^ " ; " ^ string_of_expr e2 ^ " ; " ^ *)
+      (* string_of_expr e3  ^ ") " ^ string_of_stmt s *)
+  (* | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s *)
+  | Declaration(decl) -> string_of_decl decl
+  | Transition(id, expr) -> string_of_ident id ^ " <- (" ^ string_of_expr expr ^ ")"
 
-let string_of_vdecl id = "int " ^ id ^ ";\n"
 
-let string_of_fdecl fdecl =
-  fdecl.fname ^ "(" ^ String.concat ", " fdecl.formals ^ ")\n{\n" ^
-  String.concat "" (List.map string_of_vdecl fdecl.locals) ^
-  String.concat "" (List.map string_of_stmt fdecl.body) ^
+
+let rec string_of_node = function
+  Node(id, stmtlist) -> string_of_ident id ^ " {\n" ^
+    String.concat "\n" (List.map string_of_stmt stmtlist) ^ "\n}"
+
+let string_of_formal = function
+  Formal(dt, id) -> string_of_datatype dt ^ " " ^ string_of_ident id
+
+
+
+(* let string_of_vdecl id = "int " ^ id ^ ";\n" *)
+
+let string_of_dfadecl dfadecl =
+  string_of_datatype dfadecl.return ^ " " ^ string_of_ident dfadecl.fname ^ "(" ^ String.concat ", " (List.map string_of_formal dfadecl.formals) ^ ")\n{\n" ^
+  String.concat "" (List.map string_of_decl dfadecl.var_body) ^
+  String.concat "" (List.map string_of_node dfadecl.node_body) ^
   "}\n"
 
-let string_of_program (vars, funcs) =
-  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
-  String.concat "\n" (List.map string_of_fdecl funcs)
+let string_of_program (program) =
+  String.concat "" (List.map string_of_dfadecl program)
 
