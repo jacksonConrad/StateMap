3d6f4ec195a9396d9d61d0745ebf22ee9dd34ba9
Nero144 <ofinard@gmail.com>
2014-12-10 21:18:38 -0500

    changed the python template

--- sample_programs/python_repr.py
+++ sample_programs/python_repr.py
@@ -17,3 +17,118 @@ def subdfa1():
 	
 def __main__:
 	return main()
+
+
+
+#concurrent(dfa1,dfa2,dfa3)
+
+class dfa1:
+    def __init__(self):
+        self.stackDum = list()
+        self.now = self.start
+        self.nexT = None
+        self.returnIt = None
+    def start(self):
+        if len(self.stackDum) < 10:
+            self.nexT = self.state2
+        else:
+            self.nexT = self.end
+
+    def state2(self):
+        self.stackDum.add(0)
+        self.nexT = self.start
+    
+    def end(self):
+        self.returnIt = len(self.stackDum)
+        self.nexT = None
+
+class dfa2:
+    def __init__(self):
+        self.stackDum = list()
+        self.now = self.start
+        self.nexT = None
+        self.returnIt = None
+    def start(self):
+        if len(self.stackDum) < 10:
+            self.nexT = self.state2
+        else:
+            self.nexT = self.end
+
+    def state2(self):
+        self.stackDum.add(0)
+        self.nexT = self.start
+    
+    def end(self):
+        self.returnIt = len(self.stackDum)
+        self.nexT = None
+
+
+
+class dfa3:
+    def __init__(self):
+        self.stackDum = list()
+        self.now = self.start
+        self.nexT = None
+        self.returnIt = None
+    def start(self):
+        if len(self.stackDum) < 10:
+            self.nexT = self.state2
+        else:
+            self.nexT = self.end
+
+    def state2(self):
+        self.stackDum.add(0)
+        self.nexT = self.start
+    
+    def end(self):
+        self.returnIt = len(self.stackDum)
+        self.nexT = None
+
+
+class main:
+    def __init__(self,args1,args2,args3):
+        self.args1 = args1#alt style
+        self.args2 = args2#alt style
+        self.args = [args1,args2,args3] #have this, even if blank
+        self.stack1 = range(33)
+        self.stack2 = list()
+        self.now = self.start
+        self.nexT = None
+        self.returnIt = None
+        while not returnIt: #NOTE: THIS IS ONLY TRUE FOR MAIN DO NOT DO FOR OTHER DFAS
+            self.now()
+            self.now = self.nexT()
+
+   def start(self):
+       #run stuff
+       if 1 < 2:
+           self.nexT = self.state2
+       else:
+           self.nexT = self.quit
+   
+    def quit(self):
+        self.returnIt = "fuckAll"
+        self.nexT = None
+
+    def state2(self):
+        self.stack2.add(self.stack1().pop(0))
+        if len(self.stack2) < 10:
+            self.nexT = self.start
+        else:
+            #concurrent(dfa1,dfa2,dfa2)
+            dfasToRun = [dfa1(), dfa2(), dfa3()]
+            while not arg1.returnIt or not arg2.returnIt or not arg3.returnIt:
+                for dfa in dfasToRun:
+                    dfa.now()
+                for dfa in dfasToRun:
+                    dfa.now = dfa.nexT
+
+            self.nexT = self.quit
+
+    def state3(self):
+        i = dfa2().returnIt
+        return "moreThanFuckAll" + i
+
+
+
+
