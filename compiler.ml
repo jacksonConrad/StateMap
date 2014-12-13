e58e6da6df8cad2c04eb29f355cc56acbbe27b9d
Nero144 <ofinard@gmail.com>
2014-12-13 18:43:37 -0500

    changed compiler stuffs

--- compiler.ml
+++ compiler.ml
@@ -3,8 +3,21 @@ open Gen_python
 
 let _ =
   (* let lexbuf = Lexing.from_channel stdin in *)
-  let lexbuf = Lexing.from_string "int DFA main(){start{print(\"Hello World!\");
-  return 3; start <- *;}}" in
+    let lexbuf = Lexing.from_string 
+    "int DFA main(){
+        int counter = 0; 
+        start{
+            print(\"Hello World!\");
+            state3<- (counter > 10);
+            state2<-*;}
+        state2{
+            print(\"state2\n\");
+            counter=(counter + 1);
+            start<-*;}
+        state3{
+            print(\"state3\n\");
+            return 10;}
+}" in
   let ast = Parser.program Scanner.token lexbuf in
   let sast = Semantic_check.check_program ast in
   let code = gen_program sast in
