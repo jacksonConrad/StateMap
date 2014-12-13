e2c22fbd48004d456cd76151e900bb7ba703e302
Zuokun Yu <zy2170@columbia.edu>
2014-12-13 18:42:47 -0500

    Mutually exclusive return/transition

--- compiler.ml
+++ compiler.ml
@@ -4,7 +4,7 @@ open Gen_python
 let _ =
   (* let lexbuf = Lexing.from_channel stdin in *)
   let lexbuf = Lexing.from_string "int DFA main(){start{print(\"Hello World!\");
-  return 3; start <- *;}}" in
+  return 3;}}" in
   let ast = Parser.program Scanner.token lexbuf in
   let sast = Semantic_check.check_program ast in
   let code = gen_program sast in
