60fb83873ab7d88d6eaf4fed4fa616259e371ca9
Nero144 <ofinard@gmail.com>
2014-12-14 16:52:43 -0500

    fixed the issue with all locals being seen as dfa scope, added push pop and peek and they work, and added state to the list of predefined funcs/dfas

--- tests/OrenSingleDFATest1.ml
+++ tests/OrenSingleDFATest1.ml
@@ -4,12 +4,10 @@ open Gen_python
 let _ =
   (* let lexbuf = Lexing.from_channel stdin in *)
     let lexbuf = Lexing.from_string 
-"int DFA main(){
+    "int DFA main(){
         int counter = 0; 
-        stack<int> countDown;
         start{
             print(\"Hello World!\");
-
             state3<- (counter > 10);
             state2<-*;}
         state2{
@@ -19,31 +17,8 @@ let _ =
             start<-*;}
         state3{
             print(\"state3\\n\");
-            //print(red());
-            state4<- (counter < 1);
-            counter = counter - 1;
-            countDown.push(1);
-            state3 <-*;
-            }
-        state4{
-            state5<-(countDown.peek == EOS);
-            print(\"popping like cray\");
-            countDown.pop;
-            state4<-*;
-            }
-        state5{
-            print(\"Donezoes\");
-            return 0;
-            }
-}
-/*string DFA red() {
-    string fire = \"Fire\";
-    start{
-        print(\"Called red()\");
-        return fire;
-        }
-}*/
-" in
+            return 10;}
+}" in
   let ast = Parser.program Scanner.token lexbuf in
   let sast = Semantic_check.check_program ast in
   let code = gen_program sast in
