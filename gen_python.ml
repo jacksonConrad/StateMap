e1f5f52c5d0aa519766ae17d9a87dd321e71313b
Jackson Foley <jacksoncfoley@gmail.com>
2014-12-16 13:59:52 -0500

    boolean binops now return 1 or 0 instead of True or False

--- gen_python.ml
+++ gen_python.ml
@@ -125,7 +125,7 @@ let rec gen_sexpr sexpr = match sexpr with
 | SStringLit(s, d) -> "\"" ^ s ^ "\""
 | SVariable(sident, d) -> get_sident_name sident
 | SUnop(unop, sexpr, d) -> gen_unop unop ^ "(" ^ gen_sexpr sexpr ^ ")"
-| SBinop(sexpr1, binop, sexpr2, d) -> "(" ^ gen_sexpr sexpr1 ^ gen_binop binop ^
+| SBinop(sexpr1, binop, sexpr2, d) -> "int(" ^ gen_sexpr sexpr1 ^ gen_binop binop ^
     gen_sexpr sexpr2 ^ ")" 
 | SPeek(sident,dt) -> let stackName = get_sident_name sident in
     "(" ^ stackName ^ "[0] if len(" ^ stackName ^") else EOS())"
