6c56b6cc7b8cc644e9cd3277ed48b87be2fae9d1
Zuokun Yu <zy2170@columbia.edu>
2014-12-17 16:58:58 -0500

    Variadic output file

--- compiler.ml
+++ compiler.ml
@@ -1,10 +1,11 @@
 open Semantic_check
 open Gen_python
+open Sys
 
 let _ =
   let lexbuf = Lexing.from_channel stdin in
   let ast = Parser.program Scanner.token lexbuf in
   let sast = Semantic_check.check_program ast in
   let code = gen_program sast in
-  let output = open_out "output.py" in
+  let output = open_out (Sys.argv.(1) ^ ".py") in
   output_string output code
--- unit_tests.sh
+++ unit_tests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #script used for reg testing
 COMPILER="./compiler"
-COMPFILE='output.py'
 LOGFILE='log.txt'
 
 rm -f "$LOGFILE" &>/dev/null
@@ -9,16 +8,18 @@ for TESTFILE in ./tests/*.sm;
 do
  	echo "	TESTING $TESTFILE" | tee -a "$LOGFILE"
 	LEN=$((${#TESTFILE}-3))
+  FILENAME="${TESTFILE:0:$LEN}"
 	OUTFILENAME="${TESTFILE:0:$LEN}.output"
 	TESTFILENAME="${TESTFILE:0:$LEN}.out"
+  echo $FILENAME
 	echo "Compiling ... " >> "$LOGFILE"
-	("$COMPILER" < "$TESTFILE") 2>> "$LOGFILE"
+	("$COMPILER" "$FILENAME" < "$TESTFILE") 2>> "$LOGFILE"
 
 	# if compilation succeeds, run output.py.
-	if (find output.py &>/dev/null)
+	if (find $FILENAME.py &>/dev/null)
 	then
 		echo "Python runtime output:" >> "$LOGFILE"
-		(python "$COMPFILE" > "$OUTFILENAME") 2>> "$LOGFILE"
+		(python "$FILENAME.py" > "$OUTFILENAME") 2>> "$LOGFILE"
 		echo "Diff:\n" >> "$LOGFILE"
 		touch "$OUTFILENAME"
 		if (diff "$OUTFILENAME" "$TESTFILENAME" >/dev/null) 
@@ -31,7 +32,6 @@ do
 	else
 		echo "BAD!\nCompilation of $TESTFILE FAILED" | tee -a "$LOGFILE"
 	fi
-	touch output.py
-	rm '$COMPFILE' "$OUTFILENAME" &>/dev/null
+	rm "${FILENAME}.py" "$OUTFILENAME" &>/dev/null
 done
 exit 0
